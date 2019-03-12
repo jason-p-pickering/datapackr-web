393e7b813de59e4f2504db4c99114b602f004b72
Jason Pickering <jason.p.pickering@gmail.com>
2019-03-12 11:13:13 +0100

    Minor change to operator

--- utils.R
+++ utils.R
@@ -88,7 +88,7 @@ validatePSNUData<-function(d) {
   diff<-gsub(" <= ","/",vr_violations$formula)
   
   vr_violations$diff<-sapply(diff,function(x) { round( ( eval(parse(text=x)) -1 ) * 100 , 2) })
-  vr_violations %>% dplyr::filter(diff >= 5)
+  vr_violations %<>% dplyr::filter(diff >= 5)
   d$datim$vr_rules_check <-vr_violations[,c("name","ou_name","mech_code","formula","diff")]
   
   d
