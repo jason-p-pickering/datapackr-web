61598743e3f8e0b8d7773877445063eaeaf84725
Jason Pickering <jason.p.pickering@gmail.com>
2019-03-18 16:52:27 +0200

    Temporary hack for results status.

--- utils.R
+++ utils.R
@@ -117,7 +117,20 @@ adornMechanisms <- function(d) {
 
 adornMERData <- function(df) {
   
-   df %<>%  
+  suppressWarnings( df <- df  %>% tidyr::separate(
+    indicatorCode,
+    into = c(
+      "indicator",
+      "numeratordenom",
+      "disaggregate",
+      NA,
+      "otherdisaggregate"
+    ),
+    sep = "\\.", 
+    remove = FALSE )) 
+  
+  
+   df %<>% dplyr::select(-indicator,-numeratordenom,-disaggregate) %>% 
      dplyr::left_join(datapackr::PSNUxIM_to_DATIM %>%
                      dplyr::filter(dataset == "MER") %>%
                      dplyr::select(-sheet_name, -typeOptions, -dataset),
@@ -131,14 +144,6 @@ adornMERData <- function(df) {
         otherdisaggregate %in% c("NewPos", "KnownPos", "Positive") ~ "Positive",
         otherdisaggregate %in% c("NewNeg", "Negative")             ~ "Negative",
         otherdisaggregate == "Unknown"                             ~ "Unknown"
-      ),
-      otherdisaggregate = ifelse(
-        !stringr::str_detect(indicator, "STAT") &
-          otherdisaggregate %in% c("NewPos", "Positive",
-                                   "NewNeg", "Negative",
-                                   "Unknown"),
-        as.character(NA),
-        otherdisaggregate
       )
     )
   
