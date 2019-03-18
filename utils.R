8417f65eb723d940397eb2b3cf25a99e0fe59ef1
Jason Pickering <jason.p.pickering@gmail.com>
2019-03-18 09:38:28 +0200

    Add color for unknown status on summary graph

--- utils.R
+++ utils.R
@@ -189,7 +189,7 @@ modalitySummaryChart <- function(df) {
     geom_col() +
     scale_y_continuous(labels = scales::comma) +
     coord_flip() +
-    scale_fill_manual(values = c("#548dc0", "#59BFB3")) +
+    scale_fill_manual(values = c(	"#948d79", "#548dc0", "#59BFB3")) +
     labs(y = "", x = "",
          title = "COP19/FY20 Testing Targets",
          subtitle = "modalities ordered by total tests") +
