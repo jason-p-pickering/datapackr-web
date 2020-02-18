9ff8cacf1418065872d507d358d05fb70361b719
Jason P. Pickering <jason.p.pickering@gmail.com>
2020-02-18 07:07:30 +0100

    Add prioritization from the datapack as a dimension.

--- utils.R
+++ utils.R
@@ -521,13 +521,34 @@ adornPSNUs<-function(df) {
   
   df %>% dplyr::left_join(getPSNUList(), by = c("psnuid" = "psnu_uid"))
   
+  prio_defined<-tibble::tribble(
+    ~value,~prioritization,
+    1,"Scale-Up: Saturation",
+    4 ,"Sustained",
+    0 ,"No Prioritization",
+    6, "Sustained: Commodities" ,
+    2, "Scale Up: Aggressive"  ,   
+    5, "Centrally Supported",
+    7,  "Attained" ,
+    8, "Not PEPFAR Supported" )
+  
+  
+  #We need to add the prioritization as a dimension here
+  prio <- d$data$SUBNAT_IMPATT %>% 
+    dplyr::filter(indicator_code == "IMPATT.PRIORITY_SNU.T") %>% 
+    dplyr::select(psnuid,value) %>% 
+    dplyr::left_join(prio_defined,by="value")
+  
+  df %>% dplyr::left_join(prio,by="psnuid") %>% 
+    dplyr::mutate(prioritization = case_when(is.na(prioritization) ~ "No Prioritization",
+                                      TRUE ~ prioritization ))
+  
 }
 
 prepareFlatMERExport<-function(vr) {
   
   vr$data$distributedMER %>% 
     dplyr::mutate(timestamp = format(Sys.time(),"%Y-%m-%d %H:%M:%S"),
-                  prioritization = NA,
                   fiscal_year = "FY21") %>% 
     dplyr::select( ou,
                    ou_id,
@@ -687,4 +708,5 @@ validationSummary<-function(vr,config) {
   
   
 }
+
 
